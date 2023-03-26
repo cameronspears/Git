1b6a523a9b67b52d590d5d616e308e6480c4e1ac
cameronspears <cameronspears94@gmail.com>
2023-03-25 19:08:15 -0500

    Update DownloadInstallGit.py

--- DownloadInstallGit.py
+++ DownloadInstallGit.py
@@ -5,26 +5,35 @@ import subprocess
 import sys
 from pathlib import Path
 
-# Function to get the latest Git release and 64-bit download URL from GitHub API
+def main():
+    if platform.system() != "Windows":
+        print("This script is designed for Windows systems.")
+        sys.exit(1)
+
+    if is_git_installed():
+        handle_existing_git_installation()
+    else:
+        install_latest_git_version()
+
 def get_latest_git_release():
     api_url = "https://api.github.com/repos/git-for-windows/git/releases/latest"
     response = requests.get(api_url)
     response.raise_for_status()
     data = response.json()
 
-    asset_64bit = None
-    # Find the 64-bit installer asset in the release assets
-    for asset in data["assets"]:
-        if "64-bit.exe" in asset["name"]:
-            asset_64bit = asset
-            break
+    asset_64bit = find_64bit_asset(data["assets"])
 
     if asset_64bit is None:
         raise Exception("64-bit Git installer not found in release assets")
 
     return data["tag_name"], asset_64bit["browser_download_url"]
 
-# Function to download the Git installer to a specified save_path
+def find_64bit_asset(assets):
+    for asset in assets:
+        if "64-bit.exe" in asset["name"]:
+            return asset
+    return None
+
 def download_git_installer(url, save_path):
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -33,11 +42,10 @@ def download_git_installer(url, save_path):
         for chunk in response.iter_content(chunk_size=8192):
             installer_file.write(chunk)
 
-# Function to install Git using the downloaded installer
 def install_git(installer_path):
     installation_args = [
         installer_path,
-        "/SILENT",
+        "/VERYSILENT",
         "/NORESTART",
         "/NOCANCEL",
         "/SP-",
@@ -50,34 +58,42 @@ def install_git(installer_path):
     result = subprocess.run(installation_args, check=True)
     return result.returncode == 0
 
-# Function to check if Git is installed
 def is_git_installed():
     try:
-        subprocess.run(["git", "--version"], check=True)
+        subprocess.run(["git", "--version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
         return True
     except FileNotFoundError:
         return False
 
-# Function to get the installed Git version
 def get_git_version():
     result = subprocess.run(["git", "--version"], capture_output=True, text=True, check=True)
-    return result.stdout.strip()
+    version_str = result.stdout.strip().split()[-1]
+    return version_str
 
-if __name__ == "__main__":
-    if platform.system() != "Windows":
-        print("This script is designed for Windows systems.")
-        sys.exit(1)
+def handle_existing_git_installation():
+    current_version = get_git_version()
+    print(f"Git is already installed: {current_version}")
 
-    if is_git_installed():
-        current_version = get_git_version()
-        print(f"Git is already installed: {current_version}")
+    latest_version, url = get_latest_git_release()
 
-        latest_version, url = get_latest_git_release()
+    latest_version = latest_version.lstrip('v') # remove 'v' prefix from latest version
+    if current_version == latest_version:
+        print("Git is up to date!")
+        repair_choice = input("Do you want to repair the Git installation? (y/n): ")
+        if repair_choice.lower() != "y":
+            sys.exit(0)
+    else:
         update_choice = input(f"Do you want to update Git to the latest version ({latest_version})? (y/n): ")
         if update_choice.lower() != "y":
             sys.exit(0)
-    else:
+
+    install_latest_git_version(url)
+
+def install_latest_git_version(url=None):
+    if url is None:
         latest_version, url = get_latest_git_release()
+    else:
+        _, latest_version = get_latest_git_release()
 
     installer_name = "GitInstaller.exe"
     installer_path = os.path.join(os.getcwd(), installer_name)
@@ -101,3 +117,6 @@ if __name__ == "__main__":
     finally:
         if Path(installer_path).is_file():
             os.remove(installer_path)
+
+if __name__ == "__main__":
+    main()
